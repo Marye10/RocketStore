b1c5d80b0c5b578513185ba1dedad7a0d1fa0b02
Marye10 <marianaecpe10@gmail.com>
2021-10-23 19:37:21 -0500

    Corrección de unos errores

--- RocketStore/Program.cs
+++ RocketStore/Program.cs
@@ -18,29 +18,7 @@ namespace RocketStore
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            //Calculadora
-            Console.WriteLine("Elige una opción\n" +
-            "\n1. Suma" + "\n2. Resta" +
-            "\n3. Multiplicación" + "\n4. División" +
-            "\n5. Salir");
 
-            String s1 = null;
-            s1 = Console.ReadKey().ToString();
-            switch (s1)
-            {
-                case "1":
-                    Console.WriteLine("Opción Suma");
-                    break;
-                case "2":
-                    Console.WriteLine("Opción Resta");
-                    break;
-                case "3":
-                    Console.WriteLine("Opción Multiplicación");
-                    break;
-                case "4":
-                    Console.WriteLine("Opción Dvisión");
-                    break;
-            }
 
         }
     }
